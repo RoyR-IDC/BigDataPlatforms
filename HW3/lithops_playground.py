143582e2f4f22e931930162065ce07d75b90cb6e
RoyR-IDC <93724902+RoyR-IDC@users.noreply.github.com>
2022-01-06 20:15:10 +0200

    Fixed security leaks

--- HW3/lithops_playground.py
+++ HW3/lithops_playground.py
@@ -2,17 +2,17 @@ import lithops
 
 temp_from_bucket_credentials = {
     {
-        "apikey": "VqT59Ip4_SNWh9HYbN-cGHbbc1Yq2MRTnIcOscuW06lL",
+        "apikey": "...",
         "cos_hmac_keys": {
-            "access_key_id": "934602118fbf401e9ed3ed9a4c3d3d30",
-            "secret_access_key": "a064c5b8688dfb4bb5c07e9fb4549dfc4bd33c19bdb48780"
+            "access_key_id": "...",
+            "secret_access_key": "..."
         },
         "endpoints": "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints",
-        "iam_apikey_description": "Auto-generated for key crn:v1:bluemix:public:cloud-object-storage:global:a/743281a5dfd447168259c9e2c5192322:d40d09c4-1f3b-4ac3-9484-20bd3c030ce9:resource-key:93460211-8fbf-401e-9ed3-ed9a4c3d3d30",
+        "iam_apikey_description": "...",
         "iam_apikey_name": "Service credentials-1",
         "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
-        "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/743281a5dfd447168259c9e2c5192322::serviceid:ServiceId-00211bf8-8bda-4808-a457-84ab15611ebf",
-        "resource_instance_id": "crn:v1:bluemix:public:cloud-object-storage:global:a/743281a5dfd447168259c9e2c5192322:d40d09c4-1f3b-4ac3-9484-20bd3c030ce9::"
+        "iam_serviceid_crn": "...",
+        "resource_instance_id": "..."
     }
 }
 
@@ -20,12 +20,12 @@ config = {'lithops': {'backend': 'ibm_cf', 'storage': 'ibm_cos'},
 
           'ibm_cf': {'endpoint': 'https://eu-de.functions.cloud.ibm.com',
                      'namespace': 'Namespace-H1x',
-                     'api_key': 'ApiKey-c4feadee-115e-4b91-a38a-7926d3cdf168',
+                     'api_key': '...',
                      },
           'ibm_cos': {'storage_bucket': 'cloud-object-storage-a9-cos-standard-p24',
                       'region': 'eu-de',
-                      "access_key": "934602118fbf401e9ed3ed9a4c3d3d30",
-                      "secret_key": "a064c5b8688dfb4bb5c07e9fb4549dfc4bd33c19bdb48780"
+                      "access_key": "...",
+                      "secret_key": "..."
                       }
           }
 
